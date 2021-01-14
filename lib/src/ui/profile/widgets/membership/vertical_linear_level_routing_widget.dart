eedddea0682259a45c5938637ffcf1f81a59c50f
Mohammad Shamsi <mshamsi502@gmail.com>
2021-01-14 11:08:39 +0330

    test

--- lib/src/ui/profile/widgets/membership/vertical_linear_level_routing_widget.dart
+++ lib/src/ui/profile/widgets/membership/vertical_linear_level_routing_widget.dart
@@ -484,6 +484,7 @@ class _VerticalLinearLevelRoutingWidgetState
     );
   }
 
+// ? test
   double findPercentProgressIndicator(int moneyBuying) {
     double percentProgressIndicator = 0.05;
     // print('111 :$percentProgressIndicator');
